a498596112b1befcef75357f78314e67c73a28b5
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2021-07-25 03:20:23 +0900

    :+1: Reading User Input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
+
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b0001_1111 // 先頭3bitが0だと、ctrl-keyになる
+}
+
+fn die(e: std::io::Error) {
+    panic!(e);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _stdout = stdout().into_raw_mode().unwrap();
+
+    for b in io::stdin().bytes() {
+        match b {
+            Ok(b) => {
+                let c = b as char;
+                if c.is_control() { // tabとかdeleteとか. ASCII codes 0-31 and 127
+                    println!("\r{:?}\r", b);
+                } else {
+                    println!("{:?} ({})\r", b, c);
+                }
+                println!("{:#b}\r", b);
+                if b == to_ctrl_byte('q') {
+                    break;
+                }
+            }
+            Err(err) => die(err),
+        }
+
+    }
 }
