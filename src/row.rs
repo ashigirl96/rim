e763569f69f211d6f58e33192854eec69e08bad6
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2021-07-31 22:11:37 +0900

    :+1: unicodeに対応した

--- src/row.rs
+++ src/row.rs
@@ -1,29 +1,52 @@
 use std::cmp;
 use std::str::FromStr;
 use std::string::ParseError;
+use unicode_segmentation::UnicodeSegmentation;
 
 pub struct Row {
     string: String,
+    len: usize,
 }
 
 impl FromStr for Row {
     type Err = ParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Self {
+        let mut row = Self {
             string: String::from(s),
-        })
+            len: 0,
+        };
+        row.update_length();
+        Ok(row)
     }
 }
 
 impl Row {
+    // 渡されたstartからendまでの行の文字列を返す。
     pub fn render(&self, start: usize, end: usize) -> String {
         let end = cmp::min(end, self.string.len());
         let start = cmp::min(start, end);
-        self.string.get(start..end).unwrap_or_default().to_string()
+        // self.string.get(start..end).unwrap_or_default().to_string()
+        let mut result = String::new();
+        for grapheme in self.string[..]
+            .graphemes(true)
+            .skip(start)
+            .take(end - start)
+        {
+            if grapheme == "\t" {
+                result.push_str("  ");
+            } else {
+                result.push_str(grapheme);
+            }
+        }
+        result
     }
 
     pub fn len(&self) -> usize {
-        self.string.len()
+        self.len
+    }
+
+    fn update_length(&mut self) {
+        self.len = self.string[..].graphemes(true).count();
     }
 }
